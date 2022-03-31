f31dc5e64a81f26c434f3f33da43dab118d86eb7
HodaeSsi <kfdream123@gmail.com>
2022-03-31 20:04:27 +0900

    Initial commit

---
+++ src/main/java/com/hodaessi/spring/studyolle_clone/StudyolleCloneApplication.java
@@ -0,0 +1,13 @@
+package com.hodaessi.spring.studyolle_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyolleCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyolleCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hodaessi/spring/studyolle_clone/StudyolleCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodaessi.spring.studyolle_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
